1d7a7ed2062302647ec4e24a835230058d91ed70
Tiago Simões <tiago.simoes@uni-foundation.eu>
2021-02-09 17:11:58 +0100

    Added a test form for the index endpoint

---
+++ modules/ewp_institutions_get/src/Form/TestForm.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Drupal\ewp_institutions_get\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
+
+class TestForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'ewp_institutions_get_test_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Load the settings.
+    $config = \Drupal::config('ewp_institutions_get.settings');
+
+    // Index endpoint field.
+    $form['index_endpoint'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Index endpoint'),
+      '#default_value' => $config->get('ewp_institutions_get.index_endpoint'),
+      '#description' => $this->t('External API endpoint that returns the main index.'),
+      '#disabled' => TRUE,
+      '#weight' => '-9',
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+      '#weight' => '-7',
+    ];
+
+    $form['actions']['call'] = [
+      '#type' => 'button',
+      '#value' => $this->t('Perform API request'),
+      '#attributes' => [
+        'class' => [
+          'button--primary',
+        ]
+      ],
+      '#states' => [
+        'disabled' => [
+          ':input[name="index_endpoint"]' => ['value' => ''],
+        ],
+      ],
+      '#ajax' => [
+        'callback' => '::performRequest',
+      ],
+    ];
+
+    $form['message'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Request and response'),
+    ];
+
+    $form['message']['markup'] = [
+      '#type' => 'markup',
+      '#markup' => '<div class="result_message"></div>',
+      '#weight' => '-6',
+    ];
+
+    return $form;
+  }
+
+  /**
+  * Make the API call
+  */
+  public function performRequest(array $form, FormStateInterface $form_state) {
+    $endpoint = $form_state->getValue('index_endpoint');
+
+    // Initialize an HTTP client
+    $client = \Drupal::httpClient();
+    $response = NULL;
+
+    // Build the HTTP request
+    try {
+      $request = $client->get($endpoint);
+      $response = $request->getBody();
+    } catch (GuzzleException $e) {
+      $response = $e->getResponse()->getBody();
+    } catch (Exception $e) {
+      watchdog_exception('ewp_institutions_get', $e->getMessage());
+    }
+
+    $decoded = json_decode($response);
+    $encoded = json_encode($decoded, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    $message = ($response) ? htmlspecialchars($encoded) : 'Nothing to display.' ;
+
+    $ajax_response = new AjaxResponse();
+    $ajax_response->addCommand(
+     new HtmlCommand(
+       '.result_message', '<hr>' .
+       '<p>' . t('Request') . ': <code>GET ' . $endpoint . '</code></p>' .
+       '<p>' . t('Response') . ':</p>' .
+       '<pre>' . $message . '</pre>'),
+    );
+    return $ajax_response;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Nothing to do here...
+  }
+
+}
