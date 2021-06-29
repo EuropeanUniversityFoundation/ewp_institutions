f7bfa58b73f01f7e446e8336075a8512b17295bc
Tiago Simões <tiago.simoes@uni-foundation.eu>
2021-06-29 19:42:13 +0200

    Massive changes to InstitutionSelectWidget - STILL FAILING

--- modules/ewp_institutions_ref/src/Plugin/Field/FieldWidget/InstitutionSelectWidget.php
+++ modules/ewp_institutions_ref/src/Plugin/Field/FieldWidget/InstitutionSelectWidget.php
@@ -21,6 +21,9 @@ use Drupal\ewp_institutions_get\InstitutionManager;
  */
 class InstitutionSelectWidget extends OptionsSelectWidget {
 
+  const INDEX_SELECT = 'index_select';
+  const HEI_SELECT = 'hei_select';
+
   /**
    * Index endpoint
    *
@@ -43,6 +46,13 @@ class InstitutionSelectWidget extends OptionsSelectWidget {
   protected $indexLabels;
 
   /**
+   * Institution list
+   *
+   * @var array
+   */
+  protected $heiList;
+
+  /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
@@ -54,18 +64,44 @@ class InstitutionSelectWidget extends OptionsSelectWidget {
       return $element;
     }
 
+    // Get the field name from this particular field definiton
+    $field_name = $items->getFieldDefinition()->getName();
+
     unset($element['#options']);
     $element['#type'] = 'value';
+    $element['#prefix'] = '<div id="target-id">';
+    $element['#suffix'] = '</div>';
 
-    // Load default values
+    // Load stored values
     $references = $items->referencedEntities();
-    $default_value = isset($references[$delta]) ? $references[$delta] : NULL;
-    $default_id = (!empty($default_value)) ? $default_value->id() : NULL;
-    $default_index = (!empty($default_value)) ? $default_value
+    $ref_value = isset($references[$delta]) ? $references[$delta] : NULL;
+    $ref_id = (!empty($ref_value)) ? $ref_value->id() : NULL;
+    $ref_index = (!empty($ref_value)) ? $ref_value
       ->get(InstitutionManager::INDEX_FIELD)->getValue()[0]['value'] : '';
-    $default_unique = (!empty($default_value)) ? $default_value
+    $ref_unique = (!empty($ref_value)) ? $ref_value
       ->get(InstitutionManager::UNIQUE_FIELD)->getValue()[0]['value'] : '';
-    dpm($default_unique);
+
+    $state_index = $form_state->getValue(self::INDEX_SELECT);
+    $default_index = (empty($state_index)) ? $ref_index : $state_index;
+
+    $state_unique = $form_state->getValue(self::HEI_SELECT);
+    $state_unique = ($state_unique === $ref_unique) ? NULL : $state_unique;
+    $default_unique = (empty($state_unique)) ? $ref_unique : $state_unique;
+
+    if (!empty($state_index) && !empty($state_unique)) {
+      $default_id = $this->getInstitutionId($state_index, $state_unique);
+    } else {
+      $default_id = $ref_id;
+    }
+
+    $element['#default_value'] = [$default_id];
+    $element['#value'] = [$default_id];
+
+    $element['select_wrapper'] = [
+      '#type' => 'fieldset',
+      '#title' => $element['#title'],
+      '#tree' => TRUE,
+    ];
 
     // Load the settings.
     $config = \Drupal::config('ewp_institutions_get.settings');
@@ -73,6 +109,129 @@ class InstitutionSelectWidget extends OptionsSelectWidget {
     $this->indexLinks = [];
     $this->indexLabels = [];
 
+    $this->buildIndexOptions();
+
+    // Build the Index
+    $element['select_wrapper'][self::INDEX_SELECT] = [
+      '#type' => 'select',
+      '#title' => $this->t('Index'),
+      // '#required' => $element['#required'],
+      '#options' => $this->indexLabels,
+      '#default_value' => $default_index,
+      '#empty_value' => '',
+      '#ajax' => [
+        'callback' => [$this, 'refreshInstitutions'],
+        'disable-refocus' => TRUE,
+        'event' => 'change',
+        'wrapper' => 'hei-select',
+      ],
+      '#attributes' => [
+        'name' => 'index_select',
+      ],
+      '#weight' => '-9',
+    ];
+
+    $this->heiList = $this->buildInstitutionOptions($default_index);
+
+    // Build the Institution list
+    $element['select_wrapper'][self::HEI_SELECT] = [
+      '#type' => 'select',
+      '#title' => $this->t('Institution'),
+      '#prefix' => '<div id="hei-select">',
+      '#suffix' => '</div>',
+      // '#required' => $element['#required'],
+      '#options' => $this->heiList,
+      '#default_value' => $default_unique,
+      '#empty_value' => '',
+      '#ajax' => [
+        'callback' => [$this, 'refreshTargetId'],
+        'disable-refocus' => TRUE,
+        'event' => 'change',
+        'wrapper' => 'target-id',
+      ],
+      '#validated' => TRUE,
+      '#states' => [
+        'disabled' => [
+          ':input[name="index_select"]' => ['value' => ''],
+        ],
+      ],
+      '#weight' => '-8',
+    ];
+
+    // dpm($element);
+    return $element;
+  }
+
+  /**
+   * Form validation handler for widget elements.
+   *
+   * @param array $element
+   *   The form element.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state.
+   */
+  public static function validateElement(array $element, FormStateInterface $form_state) {
+    $user_input = $form_state->getUserInput();
+
+    if (\array_key_exists(self::INDEX_SELECT, $user_input)) {
+      $index_key = $user_input[self::INDEX_SELECT];
+      $form_state->setValue(self::INDEX_SELECT, $index_key);
+    }
+
+    $triggeringElement = $form_state->getTriggeringElement();
+    switch ($triggeringElement['#type']) {
+      case 'select':
+        $field_name = $triggeringElement['#array_parents'][0];
+        $delta = $triggeringElement['#array_parents'][2];
+        $wrapper = $triggeringElement['#array_parents'][3];
+
+        // if (\array_key_exists($field_name, $user_input)) {
+        //   $hei_key = $user_input[$field_name][$delta][$wrapper][self::HEI_SELECT];
+        //   $form_state->setValue(self::HEI_SELECT, $hei_key);
+        // }
+        break;
+
+      default:
+        // $state_index = $form_state->getValue(self::INDEX_SELECT);
+        // $form_state->setValue(self::HEI_SELECT, $element['select_wrapper'][self::HEI_SELECT]['#value']);
+        // $state_unique = $form_state->getValue(self::HEI_SELECT);
+        dpm($form_state->getValues());
+        break;
+    }
+
+    $message = json_encode($element['#value']);
+    \Drupal::logger('ewp_institutions_ref')->notice(t('element_value: ') . $message);
+
+    parent::validateElement($element, $form_state);
+  }
+
+  /**
+  * Rebuild the list of Institutions
+  */
+  public function refreshInstitutions(array &$form, FormStateInterface $form_state) {
+    $triggeringElement = $form_state->getTriggeringElement();
+    $field_name = $triggeringElement['#array_parents'][0];
+    $delta = $triggeringElement['#array_parents'][2];
+
+    return $form[$field_name]['widget'][$delta]['select_wrapper'][self::HEI_SELECT];
+  }
+
+  /**
+  * Recalculate the target ID
+  */
+  public function refreshTargetId(array &$form, FormStateInterface $form_state) {
+    $triggeringElement = $form_state->getTriggeringElement();
+    $field_name = $triggeringElement['#array_parents'][0];
+    $delta = $triggeringElement['#array_parents'][2];
+
+    return $form[$field_name]['widget'][$delta];
+  }
+
+  /**
+  * Build the options for the Index select element
+  */
+  private function buildIndexOptions() {
+    // Prepare the Index
     if (! empty($this->indexEndpoint)) {
       $index_data = \Drupal::service('ewp_institutions_get.fetch')
         ->getUpdated(InstitutionManager::INDEX_KEYWORD, $this->indexEndpoint);
@@ -87,39 +246,19 @@ class InstitutionSelectWidget extends OptionsSelectWidget {
       $warning = $this->t("Index endpoint is not defined.");
       \Drupal::service('messenger')->addWarning($warning);
     }
+  }
 
-    $element['#default_value'] = [$default_id];
-
-    $element['select-wrapper'] = [
-      '#type' => 'fieldset',
-      '#title' => $element['#title'],
-    ];
-
-    $element['select-wrapper']['index_select'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Index'),
-      '#options' => $this->indexLabels,
-      // '#default_value' => $this->indexLabels[$default_index],
-      '#default_value' => $default_index,
-      '#empty_value' => '',
-      '#ajax' => [
-        'callback' => [$this, 'refresh'],
-        'disable-refocus' => TRUE,
-        'event' => 'change',
-      ],
-      '#attributes' => [
-        'name' => 'index_select',
-      ],
-      '#weight' => '-9',
-    ];
-
-    $endpoint = ($default_index) ? $this->indexLinks[$default_index] : '';
-
+  /**
+  * Build the options for the Institutions select element
+  */
+  private function buildInstitutionOptions($index_key) {
+    // Prepare the Institution list
     $options = ['' => '- None -'];
+    $endpoint = ($index_key) ? $this->indexLinks[$index_key] : '';
 
     if (! empty($endpoint)) {
       $json_data = \Drupal::service('ewp_institutions_get.fetch')
-        ->getUpdated($default_index, $endpoint);
+        ->getUpdated($index_key, $endpoint);
 
       if ($json_data) {
         // Build the options list
@@ -128,75 +267,24 @@ class InstitutionSelectWidget extends OptionsSelectWidget {
       }
     }
 
-
-    $element['select-wrapper']['hei_select'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Institution'),
-      '#options' => $options,
-      '#default_value' => $default_unique,
-      '#empty_value' => '',
-      '#validated' => TRUE,
-      '#states' => [
-        'disabled' => [
-          ':input[name="index_select"]' => ['value' => ''],
-        ],
-      ],
-      '#weight' => '-8',
-    ];
-
-    dpm($element);
-
-    return $element;
+    return $options;
   }
 
   /**
-   * {@inheritdoc}
-   */
-  protected function sanitizeLabel(&$label) {
-    // Select form inputs allow unencoded HTML entities, but no HTML tags.
-    $label = Html::decodeEntities(strip_tags($label));
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function supportsGroups() {
-    return TRUE;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function getEmptyLabel() {
-    if ($this->multiple) {
-      // Multiple select: add a 'none' option for non-required fields.
-      if (!$this->required) {
-        return t('- None -');
-      }
-    }
-    else {
-      // Single select: add a 'none' option for non-required fields,
-      // and a 'select a value' option for required fields that do not come
-      // with a value selected.
-      if (!$this->required) {
-        return t('- None -');
-      }
-      if (!$this->has_value) {
-        return t('- Select a value -');
-      }
-    }
-  }
-
-  /**
-  * Rebuild the form
+  * Get the Institution ID from the selected parameters
   */
-  public function refresh(array $form, FormStateInterface $form_state) {
-    // $triggering = $form_state->getTriggeringElement();
-    // $message = json_encode($triggering->getFieldStorageDefinition());
-    // $value = $form_state->getValue($triggering['#array_parents']);
-    // \Drupal::logger('ewp_institutions_ref')->notice($message);
+  private function getInstitutionId($index_key, $hei_key) {
+    // Create a new Institution if none exists with the same key
+    $hei = \Drupal::service('ewp_institutions_get.manager')
+      ->getInstitution($hei_key, $index_key);
 
-    // $form_state->setRebuild(TRUE);
-    return $form;
+    if (!empty($hei)) {
+      foreach ($hei as $id => $hei_obj) {
+        \Drupal::logger('ewp_institutions_ref')->notice(t('id: ') . $id);
+        return $id;
+      }
+    }
+
+    return NULL;
   }
 }
