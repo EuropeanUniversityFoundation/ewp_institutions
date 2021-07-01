911a1bd73001c3ce578ccf9862f618c5cbe00c1a
groszgabor <ittisvagyok@gmail.com>
2021-07-01 12:08:28 +0200

    Other ID unique formatter

--- src/Plugin/Field/FieldFormatter/OtherHeiIdUniqueFormatter.php
+++ src/Plugin/Field/FieldFormatter/OtherHeiIdUniqueFormatter.php
@@ -3,6 +3,7 @@
 namespace Drupal\ewp_institutions\Plugin\Field\FieldFormatter;
 
 use Drupal\Component\Utility\Html;
+
 use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
@@ -27,6 +28,7 @@ class OtherHeiIdUniqueFormatter extends FormatterBase {
   public static function defaultSettings() {
     return [
       'displayed_id' => 'erasmus',
+      'display_label' => false,
     ] + parent::defaultSettings();
   }
 
@@ -43,6 +45,12 @@ class OtherHeiIdUniqueFormatter extends FormatterBase {
       '#type' => 'select',
       '#options' => $types,
       '#default_value' => $this->getSetting('displayed_id'),
+    ] + parent::settingsForm($form, $form_state);
+
+    $form['display_label'] = [
+      '#title' => $this->t('Display ID type'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('display_label'),
     ] + parent::settingsForm($form, $form_state);
 
     return $form;
@@ -62,6 +70,9 @@ class OtherHeiIdUniqueFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
+
+    $elements = [];
+
     $type_manager = \Drupal::service('ewp_institutions.other_id_types');
     $types = $type_manager->getUniqueTypeList();
 
@@ -72,10 +83,13 @@ class OtherHeiIdUniqueFormatter extends FormatterBase {
         $type = (array_key_exists($key, $types)) ? $types[$key]->render() : $key ;
 
         $elements[$delta] = [
-          '#theme' => 'other_id',
+          '#theme' => 'other_id_unique',
           '#value' => $value,
-          '#type' => $type,
         ];
+
+        if($this->getSetting('display_label') == true){
+          $elements[$delta]['#type'] = $type;
+        }
       }
     }
 
