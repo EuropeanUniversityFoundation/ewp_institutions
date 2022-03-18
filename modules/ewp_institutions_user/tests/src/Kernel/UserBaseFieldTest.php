daadd6aec8849cc50bd57181ffaf9c67433a5d51
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-03-18 18:26:13 +0100

    Very minimal test coverage

---
+++ modules/ewp_institutions_user/tests/src/Kernel/UserBaseFieldTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\Tests\ewp_institutions_user\Kernel;
+
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests that the User entity has a certain base field.
+ *
+ * @group ewp_institutions_user
+ */
+class UserBaseFieldTest extends KernelTestBase {
+
+  /**
+   * Field to test.
+   */
+  const NEW_BASE_FIELD = 'user_institution';
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'user',
+    'ewp_institutions',
+    'ewp_institutions_user'
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+  }
+
+  /**
+   * Tests that the base field definitions exist.
+   */
+  public function testBaseFieldDefinitions() {
+    $fields = \Drupal::service('entity_field.manager')
+      ->getFieldDefinitions('user', 'user');
+
+    // Test whether the field exists.
+    $this->assertArrayHasKey(self::NEW_BASE_FIELD, $fields);
+    // Test whether the field is a base field.
+    $this->assertInstanceOf(BaseFieldDefinition::class, $fields[self::NEW_BASE_FIELD]);
+  }
+}
