9cc9907dd3a13d517bf566b720fae630a44d1f98
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-03-24 17:24:35 +0100

    [_user] Adds a settings form with schema, route and menu link

---
+++ modules/ewp_institutions_user/src/Form/SettingsForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\ewp_institutions_user\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'ewp_institutions_user_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Form constructor.
+    $form = parent::buildForm($form, $form_state);
+    // Default settings.
+    $config = $this->config('ewp_institutions_user.settings');
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('ewp_institutions_user.settings');
+
+    return parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'ewp_institutions_user.settings',
+    ];
+  }
+
+}
