8e9d2507118528d3d76641b579101f310070bdd8
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-03-25 13:16:43 +0100

    [_user] Settings form elements

--- modules/ewp_institutions_user/src/Form/SettingsForm.php
+++ modules/ewp_institutions_user/src/Form/SettingsForm.php
@@ -2,10 +2,53 @@
 
 namespace Drupal\ewp_institutions_user\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class SettingsForm extends ConfigFormBase {
+
+  /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   */
+  public function __construct(
+      ConfigFactoryInterface $config_factory,
+      ModuleHandlerInterface $module_handler
+  ) {
+    parent::__construct($config_factory);
+    $this->moduleHandler = $module_handler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('module_handler'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -22,6 +65,75 @@ class SettingsForm extends ConfigFormBase {
     $form = parent::buildForm($form, $form_state);
     // Default settings.
     $config = $this->config('ewp_institutions_user.settings');
+
+    // Info text.
+    $info = $this
+      ->t('These settings apply to the user\'s Institution base field.');
+
+    $form['info'] = [
+      '#type' => 'markup',
+      '#markup' => '<p>' . $info . '</p>'
+    ];
+
+    // Cardinality.
+    $form['cardinality_label'] = [
+      '#type' => 'label',
+      '#title' => $this->t('Allowed number of values'),
+    ];
+
+    $form['cardinality'] = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['inline-widget']],
+      '#attached' => ['library' => ['ewp_core/inline_widget']],
+    ];
+
+    $form['cardinality']['options'] = [
+      '#type' => 'select',
+      '#options' => [
+        $this->t('Limited'),
+        $this->t('Unlimited')
+      ],
+      '#attributes' => [
+        'name' => 'cardinality_select',
+      ],
+    ];
+
+    $form['cardinality']['number'] = [
+      '#type' => 'number',
+      '#min' => 1,
+      '#size' => 2,
+      '#states' => [
+        'visible' => [
+          ':input[name="cardinality_select"]' => ['value' => 0],
+        ],
+      ],
+    ];
+
+    // Required.
+    $form['required'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Required field')
+    ];
+
+    // Auto create.
+    $auto_create_title = $this
+      ->t("Create referenced entities if they don't already exist");
+
+    $auto_create_description = $this
+      ->t("EWP Institutions GET module must be enabled.");
+
+    $auto_create_disabled = $this->moduleHandler
+      ->moduleExists('ewp_institutions_get');
+
+    $form['auto_create'] = [
+      '#type' => 'checkbox',
+      '#title' => $auto_create_title,
+      '#disabled' => $auto_create_disabled
+    ];
+
+    if ($auto_create_disabled) {
+      $form['auto_create']['#description'] = $auto_create_description;
+    }
 
     return $form;
   }
