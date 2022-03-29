6375c72999c980eb0ee64e75648e1464e383a68e
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-03-29 11:58:32 +0200

    [_lookup] New lookup manager service

--- modules/ewp_institutions_lookup/src/InstitutionLookupManager.php
+++ modules/ewp_institutions_lookup/src/InstitutionLookupManager.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\ewp_institutions_lookup;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\ewp_institutions_get\DataFormatter;
+use Drupal\ewp_institutions_get\InstitutionManager;
 use Drupal\ewp_institutions_get\JsonDataFetcher;
 use Drupal\ewp_institutions_get\JsonDataProcessor;
 
@@ -12,30 +14,48 @@ use Drupal\ewp_institutions_get\JsonDataProcessor;
 class InstitutionLookupManager {
 
   /**
-  * Data formatting service.
-  *
-  * @var \Drupal\ewp_institutions_get\DataFormatter
-  */
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Data formatting service.
+   *
+   * @var \Drupal\ewp_institutions_get\DataFormatter
+   */
   protected $dataFormatter;
 
   /**
-  * JSON data fetching service.
-  *
-  * @var \Drupal\ewp_institutions_get\JsonDataFetcher
-  */
+   * Institution entity manager.
+   *
+   * @var \Drupal\ewp_institutions_get\InstitutionManager
+   */
+  protected $heiManager;
+
+  /**
+   * JSON data fetching service.
+   *
+   * @var \Drupal\ewp_institutions_get\JsonDataFetcher
+   */
   protected $jsonDataFetcher;
 
   /**
-  * JSON data processing service.
-  *
-  * @var \Drupal\ewp_institutions_get\JsonDataProcessor
-  */
+   * JSON data processing service.
+   *
+   * @var \Drupal\ewp_institutions_get\JsonDataProcessor
+   */
   protected $jsonDataProcessor;
 
   /**
    * The constructor.
    *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
    * @param \Drupal\ewp_institutions_get\DataFormatter $data_formatter
+   *   Data formatting service.
+   * @param \Drupal\ewp_institutions_get\InstitutionManager $hei_manager
    *   Data formatting service.
    * @param \Drupal\ewp_institutions_get\JsonDataFetcher $json_data_fetcher
    *   JSON data fetching service.
@@ -43,43 +63,44 @@ class InstitutionLookupManager {
    *   JSON data processing service.
    */
   public function __construct(
+    ConfigFactoryInterface $config_factory,
     DataFormatter $data_formatter,
+    InstitutionManager $hei_manager,
     JsonDataFetcher $json_data_fetcher,
     JsonDataProcessor $json_data_processor
   ) {
+    $this->configFactory     = $config_factory;
     $this->dataFormatter     = $data_formatter;
+    $this->heiManager        = $hei_manager;
     $this->jsonDataFetcher   = $json_data_fetcher;
     $this->jsonDataProcessor = $json_data_processor;
   }
 
   /**
-   * Looks up an Institution ID across different sources.
+   * Look up an Institution ID in a remote lookup index.
    *
    * @param string $hei_id
    *   The Institution ID.
+   *
+   * @return array $result
+   *   An array of Index key (i.e. country code) keyed by Institution ID.
    */
   public function lookup($hei_id) {
-    // code...
-  }
+    $config = $this->configFactory->get('ewp_institutions_lookup.settings');
+    $endpoint = $config->get('lookup_endpoint');
 
-  /**
-   * Looks up an Institution ID within the system.
-   *
-   * @param string $hei_id
-   *   The Institution ID.
-   */
-  public function lookupEntity($hei_id) {
-    // code...
-  }
+    $json_data = $this->jsonDataFetcher->load('lookup', $endpoint);
+    $data = $this->jsonDataProcessor->toArray($json_data);
 
-  /**
-   * Looks up an Institution ID in a remote lookup index.
-   *
-   * @param string $hei_id
-   *   The Institution ID.
-   */
-  public function lookupRemote($hei_id) {
-    // code...
+    $result = [];
+
+    foreach ($data as $idx => $item) {
+      if ($item['id'] === $hei_id) {
+        $result[$hei_id] = $item['attributes']['country'];
+      }
+    }
+
+    return $result;
   }
 
 }
