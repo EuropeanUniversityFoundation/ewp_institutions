69ac7acb84928eaf57fd2acc0fad12cb871af3cd
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-07-05 18:31:48 +0200

    Event subscriber display messages on Institution changes

--- modules/ewp_institutions_user/src/EventSubscriber/UserInstitutionChangeEventSubscriber.php
+++ modules/ewp_institutions_user/src/EventSubscriber/UserInstitutionChangeEventSubscriber.php
@@ -3,6 +3,8 @@
 namespace Drupal\ewp_institutions_user\EventSubscriber;
 
 use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\ewp_institutions_user\Event\UserInstitutionChangeEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -10,6 +12,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  * EWP Institutions User bridge event subscriber.
  */
 class UserInstitutionChangeEventSubscriber implements EventSubscriberInterface {
+
+  use StringTranslationTrait;
 
   /**
    * The messenger.
@@ -23,9 +27,15 @@ class UserInstitutionChangeEventSubscriber implements EventSubscriberInterface {
    *
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger.
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
+   *   The string translation service.
    */
-  public function __construct(MessengerInterface $messenger) {
+  public function __construct(
+    MessengerInterface $messenger,
+    TranslationInterface $string_translation
+  ) {
     $this->messenger = $messenger;
+    $this->stringTranslation = $string_translation;
   }
 
   /**
@@ -44,7 +54,26 @@ class UserInstitutionChangeEventSubscriber implements EventSubscriberInterface {
    *   The event object.
    */
   public function onUserInstitutionChange(UserInstitutionChangeEvent $event) {
-    dpm($event);
+    if (empty($event->hei_id)) {
+      $message = $this->t('User %user is not associated with an Institution.', [
+        '%user' => $event->user->label()
+      ]);
+
+      $this->messenger->addWarning($message);
+    }
+    else {
+      $hei = [];
+      foreach ($event->hei as $idx => $entity) {
+        $hei[] = $entity->label();
+      }
+
+      $message = $this->t('User %user is associated with %hei.', [
+        '%user' => $event->user->label(),
+        '%hei' => \implode(', ', $hei)
+      ]);
+
+      $this->messenger->addStatus($message);
+    }
   }
 
 }
