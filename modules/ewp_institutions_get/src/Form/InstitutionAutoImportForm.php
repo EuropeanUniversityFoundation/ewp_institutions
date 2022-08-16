c2864e5a3f22d62ef005bc6f299012e0c392d874
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-08-16 18:05:40 +0200

    Dependency injection in auto inport form (ER widget)

--- modules/ewp_institutions_get/src/Form/InstitutionAutoImportForm.php
+++ modules/ewp_institutions_get/src/Form/InstitutionAutoImportForm.php
@@ -2,16 +2,74 @@
 
 namespace Drupal\ewp_institutions_get\Form;
 
-use Drupal\Core\Form\FormStateInterface;
-use GuzzleHttp\Exception\GuzzleException;
 use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\CloseModalDialogCommand;
 use Drupal\Core\Ajax\HtmlCommand;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element\StatusMessages;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\ewp_institutions_get\Form\PreLoadForm;
 use Drupal\ewp_institutions_get\InstitutionManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class InstitutionAutoImportForm extends PreLoadForm {
+
+  use StringTranslationTrait;
+
+  /**
+   * Index endpoint.
+   *
+   * @var string
+   */
+  protected $indexEndpoint;
+
+  /**
+   * Index item links.
+   *
+   * @var array
+   */
+  protected $indexLinks;
+
+  /**
+   * Index item labels.
+   *
+   * @var array
+   */
+  protected $indexLabels;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The Institution manager service.
+   *
+   * @var \Drupal\ewp_institutions_get\InstitutionManager
+   */
+  protected $institutionManager;
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    $instance = parent::create($container);
+    $instance->entityTypeManager  = $container->get('entity_type.manager');
+    $instance->institutionManager = $container->get('ewp_institutions_get.manager');
+    $instance->requestStack       = $container->get('request_stack');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -91,12 +149,12 @@ class InstitutionAutoImportForm extends PreLoadForm {
     $options = ['' => '- None -'];
 
     if (! empty($endpoint)) {
-      $json_data = \Drupal::service('ewp_institutions_get.fetch')
+      $json_data = $this->jsonDataFetcher
         ->getUpdated($index_item, $endpoint);
 
       if ($json_data) {
         // Build the options list
-        $options += \Drupal::service('ewp_institutions_get.json')
+        $options += $this->jsonDataProcessor
           ->idLabel($json_data);
       }
     }
@@ -115,36 +173,36 @@ class InstitutionAutoImportForm extends PreLoadForm {
     $output = '';
 
     // JSON data has to be stored at this point per previous step
-    $json_data = \Drupal::service('ewp_institutions_get.fetch')
+    $json_data = $this->jsonDataFetcher
       ->load($index_item, $endpoint);
-    $hei_list = \Drupal::service('ewp_institutions_get.json')
+    $hei_list = $this->jsonDataProcessor
       ->idLabel($json_data);
 
     $hei_item = $form_state->getValue('hei_select');
 
     // Create a new Institution if none exists with the same key
-    $hei = \Drupal::service('ewp_institutions_get.manager')
+    $hei = $this->institutionManager
       ->getInstitution($hei_item, $index_item);
     if (!empty($hei)) {
       foreach ($hei as $id => $value) {
         $entity_id = $id;
       }
       $view_mode = 'full';
-      $entity = \Drupal::entityTypeManager()
+      $entity = $this->entityTypeManager
         ->getStorage(InstitutionManager::ENTITY_TYPE)
         ->load($entity_id);
-      $view_builder = \Drupal::entityTypeManager()
+      $view_builder = $this->entityTypeManager
         ->getViewBuilder(InstitutionManager::ENTITY_TYPE);
       $pre_render = $view_builder->view($entity, $view_mode);
       $html= render($pre_render);
 
       $text = $this->t('This institution is now available for selection.');
-      $modal = \Drupal::request()->query->has('modal');
+      $modal = $this->requestStack->getCurrentRequest()->query->has('modal');
       if ($modal) {
         $text .= ' ';
         $text .= $this->t('You can close this popup and find it in the list.');
       }
-      \Drupal::service('messenger')->addMessage($text);
+      $this->messenger->addMessage($text);
     }
     else {
       $html = '';
