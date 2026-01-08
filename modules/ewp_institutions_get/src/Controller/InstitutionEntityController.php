6dba294d0074bbec71b0030cbc24f9df7cdc06a8
Tiago Simões <tiago.simoes@uni-foundation.eu>
2026-01-08 16:29:52 +0100

    Simplifies dependency injection in entity controller

--- modules/ewp_institutions_get/src/Controller/InstitutionEntityController.php
+++ modules/ewp_institutions_get/src/Controller/InstitutionEntityController.php
@@ -3,17 +3,9 @@
 namespace Drupal\ewp_institutions_get\Controller;
 
 use Drupal\Core\Entity\Controller\EntityController;
-use Drupal\Core\Entity\EntityRepositoryInterface;
-use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Render\RendererInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Routing\UrlGeneratorInterface;
-use Drupal\Core\StringTranslation\TranslationInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 use Drupal\ewp_institutions_get\InstitutionManager;
 
 /**
@@ -40,66 +32,16 @@ class InstitutionEntityController extends EntityController {
   protected $institutionManager;
 
   /**
-   * Constructs an InstitutionEntityController object.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
-   *   The entity type bundle info.
-   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
-   *   The entity repository.
-   * @param \Drupal\Core\Render\RendererInterface $renderer
-   *   The renderer.
-   * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
-   *   The string translation.
-   * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
-   *   The url generator.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
-   *   The route match.
-   * @param \Symfony\Component\Routing\Matcher\UrlMatcherInterface $access_unaware_router
-   *   A router implementation which does not check access.
-   * @param \Drupal\ewp_institutions_get\InstitutionManager $institution_manager
-   *   The Institution manager service.
-   */
-  public function __construct(
-    EntityTypeManagerInterface $entity_type_manager,
-    EntityTypeBundleInfoInterface $entity_type_bundle_info,
-    EntityRepositoryInterface $entity_repository,
-    RendererInterface $renderer,
-    TranslationInterface $string_translation,
-    UrlGeneratorInterface $url_generator,
-    RouteMatchInterface $route_match,
-    UrlMatcherInterface $access_unaware_router,
-    InstitutionManager $institution_manager,
-  ) {
-    parent::__construct(
-      $entity_type_manager,
-      $entity_type_bundle_info,
-      $entity_repository,
-      $renderer,
-      $string_translation,
-      $url_generator,
-      $route_match
-    );
-    $this->accessUnawareRouter = $access_unaware_router;
-    $this->institutionManager = $institution_manager;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('entity_type.manager'),
-      $container->get('entity_type.bundle.info'),
-      $container->get('entity.repository'),
-      $container->get('renderer'),
-      $container->get('string_translation'),
-      $container->get('url_generator'),
-      $container->get('current_route_match'),
-      $container->get('router.no_access_checks'),
-      $container->get('ewp_institutions_get.manager')
-    );
+    // Instantiates this controller class.
+    $instance = parent::create($container);
+
+    $instance->accessUnawareRouter = $container->get('router.no_access_checks');
+    $instance->institutionManager = $container->get('ewp_institutions_get.manager');
+
+    return $instance;
   }
 
   /**
